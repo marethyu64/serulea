96488b57ec423ee3bb6703274f6631ccc4063c17
marethyu64 <marethyu64@gmail.com>
2025-01-20 21:50:30 -0800

    94% completion for puzzle

--- Puzzles/energy_tether.gd
+++ Puzzles/energy_tether.gd
@@ -1,53 +1,9 @@
 extends StaticBody2D
 
-const maxrange = 5000
+class_name energy_tether
 
-@export var shoot : bool = false
-@export var input : bool = false #Is the tether tethered by another tether?
-@export var output : bool = false #Is the tether outputting an energy tether?
+@export var group = 0
+@export var tethers = 0
 
-var mouse_inside = false
-var click_activated = false
-
-@onready var tether_manager = get_parent()
-
-@onready var collision: CollisionShape2D = $Line2D/Area2D/CollisionShape2D
-@onready var tether_detection: Area2D = $TetherDetection
-
-func _process(delta):
-	if Input.is_action_just_pressed("Left Click") and mouse_inside and output == false:
-		shoot = true
-	if Input.is_action_just_released("Left Click") and output == false:
-		if shoot: shoot = false	
-	handle_tether()
-
-func _on_mouse_entered() -> void: mouse_inside = true
-func _on_mouse_exited() -> void: mouse_inside = false
-
-func handle_tether():
-	if output == false and not tether_manager.puzzle_completed:
-		if shoot == true:
-			collision.shape.b = $Line2D.points[1]
-			$Line2D.visible = true
-			$TetherLead.visible = true
-			var mouse_position = get_local_mouse_position()
-			$RayCast2D.target_position = mouse_position
-			if $RayCast2D.is_colliding():
-				$TetherLead.global_position = $RayCast2D.get_collision_point()
-				$Line2D.set_point_position(1,$Line2D.to_local($TetherLead.global_position))
-			else:
-				$TetherLead.global_position = $RayCast2D.to_global($RayCast2D.target_position)
-				$Line2D.points[1] = $Line2D.to_local($TetherLead.global_position)
-		else:
-			collision.shape.b = Vector2.ZERO
-			collision.disabled = true
-			$Line2D.visible = false
-			$TetherLead.visible = false
-			$Line2D.set_point_position(1,Vector2.ZERO)
-			$RayCast2D.target_position = Vector2.ZERO
-
-
-
-func _on_tether_lead_area_entered(area: Area2D) -> void:
-	if area.name == "TetherDetection" and area != tether_detection:
-		tether_manager.tether_request.emit(self,area.get_parent())
+func _physics_process(delta: float) -> void:
+	$Label.text = str(group)
--- Puzzles/tether_manager.gd
+++ Puzzles/tether_manager.gd
@@ -5,44 +5,104 @@ signal puzzle_completion
 
 @onready var number_of_tethers = get_child_count()
 
+const TETHER = preload("res://Puzzles/tether.tscn")
+const MOUSE_DISTANCE_TO_TETHER = 60
+
 var tethers : int = 0
+var group_ticket : int = 0
+var dragging_tether : bool = false
+
+var new_tether : Node2D
+var current_energy_tether : StaticBody2D
 
 @export var puzzle_completed = false
 
+func _ready():
+	Player.character_swap.connect(character_swapped)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_released("Left Click") and not puzzle_completed: disable_tethers()
+	if Input.is_action_just_pressed("Left Click"):
+		for tether in get_children():
+			if get_global_mouse_position().distance_to(tether.global_position) < MOUSE_DISTANCE_TO_TETHER:
+				select_tether(tether)
+	if Input.is_action_just_released("Left Click") and dragging_tether:
+		dragging_tether = false
+		new_tether.queue_free()
+		new_tether = null
+		current_energy_tether = null
+	
+	if dragging_tether:
+		drag_tether()
 
+func drag_tether():
+	if not new_tether:
+		return 
 
-func _on_tether_request(tether1, tether2) -> void:
-	if tether2.input == false and tether2.output == false:
-		tether1.output = true
-		tether2.input = true
-		tethers += 1
-		tether1.get_node("TetherLead").global_position = tether2.global_position
-		tether1.get_node("Line2D").points[1] = tether1.get_node("Line2D").to_local(tether2.global_position)
-		tether2.shoot = true
+	var line_2d = new_tether.get_node("Line2D")
+	var raycast = new_tether.get_node("RayCast2D")
+	var mouse_position = new_tether.get_global_mouse_position()
+
+	raycast.target_position = new_tether.to_local(mouse_position)
+	raycast.force_raycast_update() 
+
+	if raycast.is_colliding():
+		var collision_point = raycast.get_collision_point()
+		line_2d.set_point_position(1, new_tether.to_local(collision_point))
+		var collider = raycast.get_collider()  
+		if collider and collider is energy_tether:
+			link_tether(current_energy_tether,collider)
+	else:
+		line_2d.set_point_position(1, new_tether.to_local(mouse_position))
+
+func select_tether(tether):
+	if tether.tethers < 2:
+		dragging_tether = true
+		current_energy_tether = tether
+		new_tether = TETHER.instantiate()
+		tether.add_child(new_tether)
+		new_tether.global_position = tether.global_position  
 		
-		if check_completion(): 
-			print("puzzle completed!")
-			puzzle_completion.emit()
+		var line_2d = new_tether.get_node("Line2D")
+		line_2d.set_point_position(0, Vector2.ZERO)
+		line_2d.set_point_position(1, Vector2.ZERO)  
+
+func link_tether(tether1,tether2):
+	if tether2.tethers < 2:
+		if tether1.group == 0 and tether2.group == 0:
+			group_ticket += 1
+			tether1.group = group_ticket
+			tether2.group = group_ticket 
+		elif tether1.group == tether2.group: 
+			return
+		elif tether1.group == 0 and tether2.group != 0:
+			tether1.group = tether2.group
+		elif tether1.group != 0:
+			if tether2.group == 0: 
+				tether2.group = tether1.group
+			else:
+				for tether in get_children():
+					if tether.group == tether2.group: 
+						tether.group = tether1.group
 		
+		new_tether.get_node("Line2D").set_point_position(1, new_tether.to_local(tether2.global_position))
+		tether1.tethers += 1
+		tether2.tethers += 1
+		dragging_tether = false
+		new_tether = null
+		current_energy_tether = null
+		if check_completion() == true:
+			print("finish!!!!")
 
 func check_completion():
-	var inputs = 0
-	var outputs = 0
+	var prev_group = 0
 	for tether in get_children():
-		if tether.input == true: inputs += 1
-		if tether.output == true: outputs += 1
-	if (inputs == number_of_tethers - 1) and (outputs == number_of_tethers - 1): 
-		for tether in get_children(): tether.shoot = false
-		puzzle_completed = true
-		return true
-	else: 
-		return false
+		if tether.group == 0: return false
+		if prev_group == 0 and tether.group != 0: tether.group = prev_group
+		if tether.group != prev_group: return false
+	return true
+			
 
-func disable_tethers():
-	for tether in get_children():
-		tether.output = false
-		tether.input = false
-		tether.shoot = false
+func character_swapped(swapped_character):
+	if swapped_character == "calin":
+		pass
